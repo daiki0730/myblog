fe6ae5f5ed7660ad3ba56713489de496fda2ff4f
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-10-16 16:55:24 +0900

    viewにshowファイルを作成
    showアクションに対応

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Blog Posts</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Blog Posts</h1>
+    <ul>
+      @forelse ($posts as $post)
+      <li><a href="">{{ $post->title }}</a></li>
+      @empty
+      <li>No posts yet</li>
+      @endforelse
+    </ul>
+  </div>
+</body>
+</html>
