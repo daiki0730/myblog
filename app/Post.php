727904129f587513f7a79a7e19989918de0cf309
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-10-18 16:41:18 +0900

    コメントできるようにモデルを設定
    「多」に対する設定なのでhas_manyを使用

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+
     protected $fillable = ['title','body'];
+
+  public function comments() {
+    return $this->hasMany('App\Comment');
+  }
+
 }
