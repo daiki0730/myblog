45e46dc31d2970f33fdc3e2e5cf67f1498dac8f7
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-10-18 17:06:02 +0900

    コメント時の動作を設定
    コメント機能実装のため

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
-    //
+  public function store(Request $request, Post $post) {
+    $this->validate($request, [
+      'body' => 'required'
+    ]);
+    $comment = new Comment(['body' => $request->body]);
+    $post->comments()->save($comment);
+    return redirect()->action('PostsController@show', $post);
+  }
 }
