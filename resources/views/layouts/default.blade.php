4aeadc316db42478f3e699fd153fedad9526f829
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-10-19 20:37:22 +0900

    footerを作成
    Bootstrapのクラスを利用

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -13,7 +13,7 @@
 
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-primary ">
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
       <span class="navbar-text text-white">
         <div class="title-logo">
           My Blog
@@ -24,9 +24,16 @@
         <a class="navbar-brand text-white" href="{{ url('/posts/create') }}">New Post</a>
       </div>
     </nav>
-    @yield('content')
+    <div class="main-contents">
+      @yield('content')
+    </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <footer class="footer bg-primary">
+      <div class="container">
+        <p class="text-center text-white">Copyright © 2019 Nishida Daiki.All Rights Reserv</p>
+      </div>
+    </footer>
   </body>
 </html>
