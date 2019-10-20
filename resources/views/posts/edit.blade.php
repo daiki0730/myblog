8f0b19eb2ee6d1b182b4312cfb277a3edaee436a
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-10-20 15:37:11 +0900

    編集ページのviewを完成
    bootstrapを使用

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,27 +1,33 @@
 @extends('layouts.default')
 
 @section('content')
-<h1>
-  <a href="{{ url('/') }}" class="header-menu">Back</a>
-  Edit Post
-</h1>
+
+<h2>
+  <a href="{{ url('/') }}" class="btn btn-outline-primary header-menu">Back Page</a>
+  <div class="blogposts">
+    Edit Post
+  </div>
+</h2>
+
 <form method="post" action="{{ url('/posts', $post->id) }}">
   {{ csrf_field() }}
   {{ method_field('patch') }}
   <p>
-    <input type="text" name="title" placeholder="enter title" value="{{ old('title', $post->title) }}">
+    <input type="text" name="title" placeholder="enter title" class= "title-input"value="{{ old('title', $post->title) }}">
     @if ($errors->has('title'))
     <span class="error">{{ $errors->first('title') }}</span>
     @endif
   </p>
   <p>
-    <textarea name="body" placeholder="enter body">{{ old('body', $post->body) }}</textarea>
+    <textarea name="body" placeholder="enter body" class="text-body">{{ old('body', $post->body) }}</textarea>
     @if ($errors->has('body'))
     <span class="error">{{ $errors->first('body') }}</span>
     @endif
   </p>
   <p>
-    <input type="submit" value="Update">
+    <input type="submit" class="btn btn-primary float-right" value="Update">
   </p>
+
 </form>
+
 @endsection
