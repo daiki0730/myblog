5d8ce91060255cb13fd6b20e6ee12f9ef329c328
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-10-20 15:41:36 +0900

    新規投稿ページを編集
    bootstrapを利用

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,26 +1,30 @@
 @extends('layouts.default')
 
 @section('content')
-<h1>
-  <a href="{{ url('/') }}" class="header-menu">Back</a>
-  New Post
-</h1>
+
+<h2>
+  <a href="{{ url('/') }}" class="btn btn-outline-primary header-menu">Back Page</a>
+  <div class="blogposts">
+    New Post
+  </div>
+</h2>
+
 <form method="post" action="{{ url('/posts') }}">
   {{ csrf_field() }}
   <p>
-    <input type="text" name="title" placeholder="enter title" value="{{ old('title') }}">
+    <input type="text" name="title" placeholder="title" class="title-input" value="{{ old('title') }}">
     @if ($errors->has('title'))
     <span class="error">{{ $errors->first('title') }}</span>
     @endif
   </p>
   <p>
-    <textarea name="body" placeholder="enter body">{{ old('body') }}</textarea>
+    <textarea name="body" placeholder="text" class="text-body">{{ old('body') }}</textarea>
     @if ($errors->has('body'))
     <span class="error">{{ $errors->first('body') }}</span>
     @endif
   </p>
   <p>
-    <input type="submit" value="Add">
+    <input type="submit" class="btn btn-primary float-right" value="Add">
   </p>
 </form>
 @endsection
