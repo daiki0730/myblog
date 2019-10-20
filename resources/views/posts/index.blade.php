1f33f44f5db04a9ff33742efd704263ec416ca4e
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-10-20 15:15:22 +0900

    トップページのviewを編集
    bootstrapを使用

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,21 +3,24 @@
 @section('content')
 
 <h2>
-  Blog Posts
+  <div class="blogposts">
+    Blog Posts
+  </div>
 </h2>
 
 <ul>
   @forelse ($posts as $post)
-  <li>
-    <a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a>
-    <a href="{{ action('PostsController@edit', $post) }}" class="edit">[Edit]</a>
-    <a href="#" class="del" data-id="{{ $post->id }}">[x]</a>
-    <form method="post" action="{{ url('/posts', $post->id) }}" id="form_{{ $post->id }}">
-
-      {{ csrf_field() }}
-      {{ method_field('delete') }}
-    </form>
-  </li>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">
+      <a href="{{ action('PostsController@show', $post) }}" class="title-edit">{{ $post->title }}</a>
+      <a href="#" class="btn btn-outline-primary float-right del" data-id="{{ $post->id }}">delete</a>
+      <a href="{{ action('PostsController@edit', $post) }}" class="btn btn-outline-primary float-right edit">edit</a>
+      <form method="post" action="{{ url('/posts', $post->id) }}" id="form_{{ $post->id }}">
+        {{ csrf_field() }}
+        {{ method_field('delete') }}
+      </form>
+    </li>
+  </ul>
   @empty
   <li>No posts yet</li>
   @endforelse
